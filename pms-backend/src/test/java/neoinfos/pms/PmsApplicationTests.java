caba9ad1966f248db3caea03a5dfdbec08e985e0
kimjaeik95 <wodlrdlsla@naver.com>
2026-08-02 23:10:05 +0900

    chore: 백엔드, 프론트엔드 프로젝트 초기 설정

---
+++ pms-backend/src/test/java/neoinfos/pms/PmsApplicationTests.java
@@ -0,0 +1,13 @@
+package neoinfos.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

f7e56eb27375005b414e16cf4beb66f55df3cdf4
kimjaeik95 <wodlrdlsla@naver.com>
2026-08-04 20:39:05 +0900

    chore: 프론트엔드 프로젝트 설정 및 의존성 추가

--- pms-frontend/src/router/index.ts
+++ pms-frontend/src/router/index.ts
@@ -1,8 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
-
+import CategoryListView from '@/views/CategoryListView.vue'
+import CategoryFormView from '@/views/CategoryFormView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    { path: '/', redirect: '/categories' },
+    { path: '/categories', name: 'category-list', component: CategoryListView },
+    { path: '/categories/new', name: 'category-new', component: CategoryFormView },
+    {
+      path: '/categories/:categoryNo/edit',
+      name: 'category-edit',
+      component: CategoryFormView,
+      props: true,
+    },
+  ],
 })
 
 export default router

8f299026248b63145a9791c9e12b5c8f0067569a
kimjaeik95 <wodlrdlsla@naver.com>
2026-08-05 21:45:05 +0900

    feat(api): 상품 마스터 백엔드 API 연동 함수 구현

---
+++ pms-frontend/src/api/productMasterApi.ts
@@ -0,0 +1,29 @@
+import type { PageResponse } from '@/types/common'
+import type {
+  ProductMaster,
+  ProductMasterRequest,
+  ProductMasterUpdateRequest,
+} from '@/types/productMaster'
+import http from './http'
+
+export const productMasterApi = {
+  list(page = 0, size = 20): Promise<PageResponse<ProductMaster>> {
+    return http.get('/product-masters', { params: { page, size } }).then((res) => res.data)
+  },
+
+  get(productNo: number): Promise<ProductMaster> {
+    return http.get(`/product-masters/${productNo}`).then((res) => res.data)
+  },
+
+  create(payload: ProductMasterRequest): Promise<ProductMaster> {
+    return http.post('product-masters', payload).then((res) => res.data)
+  },
+
+  update(productNo: number, payload: ProductMasterUpdateRequest): Promise<ProductMaster> {
+    return http.put(`/product-masters/${productNo}`, payload).then((res) => res.data)
+  },
+
+  remove(productNo: number): Promise<void> {
+    return http.delete(`/product-masters/${productNo}`).then(() => undefined)
+  },
+}
